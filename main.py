338d4b0ecad3c0eb9cc2da485788b5b83b7eaae6
Abdallah-Hammad <abdallah1136@hotmail.com>
2025-07-17 12:21:04 +0300

    Add document loaders and implement state management for chat model

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from langchain.chat_models import init_chat_model
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
-from langchain_community.document_loaders import ArxivLoader
+from langchain_community.document_loaders import ArxivLoader, TextLoader
+from langchain_community.document_loaders.pdf import BasePDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
 from typing_extensions import TypedDict, List
 from langchain_core.documents import Document
+from langgraph.graph import StateGraph, START
+
 
 llm = init_chat_model(model="gemma3:1b", model_provider="ollama")
 embedding = OllamaEmbeddings(model="nomic-embed-text:latest")
@@ -38,7 +41,7 @@ prompt = PromptTemplate.from_template(
     """
 )
 
-class State(TypeDict):
+class State(TypedDict):
     question: str
     context: List[Document]
     answer: str
@@ -48,6 +51,17 @@ def retreive(state : State):
     return {'context': retreived_docs}
 
 def generate(state: State):
-    pass
- 
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    message = prompt.invoke(
+        {"question":state['question'], "context":docs_content}
+    )
+    response = llm.invoke(message)
+    return {"answer":response.content}
+
+graph_builder = StateGraph(State).add_sequence([retreive, generate])
+graph_builder.add_edge(START, "retreive")
+graph = graph_builder.compile()
+
+response = graph.invoke({"question": "What is RL? be very thorrough"})
+print(response['answer'])
 
